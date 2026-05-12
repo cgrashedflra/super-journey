5e6196af813fd0f620026abcca8f092867da617d
Ashfaq <bdrasedas@gmail.com>
2026-05-12 21:52:06 +0600

    AuthForm ui completed

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default Signup;
