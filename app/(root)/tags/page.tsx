ec95857df6ac614b747d6a9cb596521b3ad6faf7
Ashfaq <bdrasedas@gmail.com>
2026-06-15 11:21:04 +0600

    Implement Tag fetch action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { getTags } from '@/lib/action/tag.action'
 
-const Tag = () => {
+const Tag = async () => {
+    const { success, data, error } = await getTags({
+        page: 1,
+        pageSize: 10,
+        query: '',
+        filter: 'popular'
+    })
+    const tags = data || {}
+    console.log("tags", JSON.stringify(tags, null, 2))
     return (
         <div>Tag</div>
     )
